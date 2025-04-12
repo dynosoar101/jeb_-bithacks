f12efb70d2674ac338dfe3947e0f17afe142c0e2
Vy Huynh <vyh.3331@gmail.com>
2025-04-12 14:19:51 -0700

    aiyah

--- jeb_BithacksRover.ino
+++ jeb_BithacksRover.ino
@@ -1,16 +1,17 @@
-#include <Arduino.h>
+/*program uses two L293D motor drivers to program 4 motors. 2 motors correspond to each driver, with one controller rear and one front wheels
 
-/*program uses two L293D motor drivers to program 4 motors. 2 motors correspond to each driver, with one controller rear and one front wheels*/
+Pin1 and Pin2 correspond to the left side of the motor driver, Pin3 and Pin4 -> right
+*/
 
 //variable declarations
 const int baudRate = 9600; // 115200 baud rate for esp
 
 //refer to L293d pinout for pin number naming
 //motor 1 input pin declarations
-const int motor1Pin1 = 2;
-const int motor1Pin2 = 3;
-// const int motor1Pin3;
-// const int motor1Pin4;
+const int motor1Pin1 = 2; //drives top left ouput pin
+const int motor1Pin2 = 3; //drives bottom left output pin
+const int motor1Pin3 = 4; //drives bottom right output pin
+const int motor1Pin4 = 5; //drives top right output pin
 // //motor 2 input pin declarations
 // const int motor2Pin1;
 // const int motor2Pin2;
@@ -23,8 +24,8 @@ void setup() {
   //set all motor pins to output
   pinMode(motor1Pin1,OUTPUT);
   pinMode(motor1Pin2,OUTPUT);
-  // pinMode(motor1Pin3,OUTPUT);
-  // pinMode(motor1Pin4,OUTPUT);
+  pinMode(motor1Pin3,OUTPUT);
+  pinMode(motor1Pin4,OUTPUT);
   // pinMode(motor2Pin1,OUTPUT);
   // pinMode(motor2Pin2,OUTPUT);
   // pinMode(motor2Pin3,OUTPUT);
