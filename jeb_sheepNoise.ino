a32f909509bb93ab99594c2303d512fc89d9b3c5
Vy Huynh <vyh.3331@gmail.com>
2025-04-12 18:07:16 -0700

    random baaaa booooo

--- jeb_sheepNoise.ino
+++ jeb_sheepNoise.ino
@@ -3,6 +3,7 @@
 //library inclusions
 #include <SoftwareSerial.h> //allows the use of GPIO pins for tx rx communication
 #include <DFRobotDFPlayerMini.h> //library for miniplayer
+#include <AlmostRandom.h>
 
 // Define the communication pins for the DFPlayer Mini
 /*uses digital pins 2 and 3 on the arduino. tx on arduino corresponds to rx
@@ -15,6 +16,8 @@ static const uint8_t RXPin = 3;
 SoftwareSerial softwareSerial(RXPin, TXPin);
 // Create a DFRobotDFPlayerMini object
 DFRobotDFPlayerMini MP3player;
+
+AlmostRandom randomLib;
 
 void setup() {
   // Initialize the serial monitor for debugging
@@ -30,25 +33,26 @@ void setup() {
   }
   
   // Set the volume (0-30)
-  
   MP3player.volume(30);
-  MP3player.play(1);
 }
 int value = 1;
 
 void loop(){
-randomChance = random(10);
- MP3player.play(value);
-Serial.println(MP3player.readCurrentFileNumber());
+  int randomChance = randomLib.getRandomInt();
+  
+  delay(5000);
+  Serial.println(randomChance);
+  if(randomChance > 50){
+    playNoiseAmbient();
+  }
 
-delay(1000);
-Serial.println(MP3player.readCurrentFileNumber());
 }
 
 /*sheep noises*/
 //plays ambient noises, sheep BAAAAA's
 void playNoiseAmbient(){
-  randomSoundFile = random(10);
+ // randomSoundFile = random(10);
+ value = 1;
   if(MP3player.readCurrentFileNumber() == -1){
     MP3player.play(value);
   }
